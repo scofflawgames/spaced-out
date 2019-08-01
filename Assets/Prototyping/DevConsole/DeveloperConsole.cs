49a2e808a1c0c4d07669d3282a42fd9c5c2bccab
scofflawgames <48799779+scofflawgames@users.noreply.github.com>
2019-07-31 21:13:41 -0400

    Init
    Init

---
+++ Assets/Prototyping/DevConsole/DeveloperConsole.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Console
+{
+    public abstract class ConsoleCommand
+    {
+        public abstract string Name { get; protected set; }
+
+        public abstract string Command { get; protected set; }
+
+        public abstract string Description { get; protected set; }
+
+        public abstract string Help { get; protected set; }
+
+        public void AddCommandToConsole()
+        {
+            string addMessage = " command has been added to the console.";
+
+            DeveloperConsole.AddCommandsToConsole(Command, this);
+            DeveloperConsole.AddStaticMessageToConsole(Name + addMessage);
+        }
+
+        public abstract void RunCommand();
+
+    }
+
+    public class DeveloperConsole : MonoBehaviour
+    {
+        public static DeveloperConsole Instance { get; private set;}
+
+        public static Dictionary<string, ConsoleCommand> Commands { get; private set;}
+
+        [Header("UI Components")]
+        public Canvas consoleCanvas;
+        public ScrollRect scrollRect;
+        public Text consoleText;
+        public Text inputText;
+        public InputField consoleInput;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                return;
+            }
+
+            Instance = this;
+            Commands = new Dictionary<string, ConsoleCommand>();
+        }
+
+        private void Start()
+        {
+            consoleCanvas.gameObject.SetActive(false);
+            CreateCommands();
+        }
+
+        private void CreateCommands()
+        {
+            CommandQuit commandQuit = CommandQuit.CreateCommand();
+        }
+
+        public static void AddCommandsToConsole(string _name, ConsoleCommand _command)
+        {
+            if (!Commands.ContainsKey(_name))
+            {
+                Commands.Add(_name, _command);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.BackQuote))
+            {
+                consoleCanvas.gameObject.SetActive(!consoleCanvas.gameObject.activeInHierarchy);
+                consoleInput.ActivateInputField();
+            }
+
+            if (consoleCanvas.gameObject.activeInHierarchy)
+            {
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    if (inputText.text != "")
+                    {
+                        AddMessageToConsole(inputText.text);
+                        ParseInput(inputText.text);
+                        consoleInput.ActivateInputField();
+                    }
+                }
+            }
+
+        }
+
+        private void AddMessageToConsole(string msg)
+        {
+            consoleText.text += msg + "\n";
+            scrollRect.verticalNormalizedPosition = 0f;
+        }
+
+        public static void AddStaticMessageToConsole(string msg)
+        {
+            DeveloperConsole.Instance.consoleText.text += msg + "\n";
+            DeveloperConsole.Instance.scrollRect.verticalNormalizedPosition = 0f;
+        }
+
+        private void ParseInput(string input)
+        {
+            string[] _input = input.Split(null);
+
+            if (_input.Length == 0 || _input == null)
+            {
+                AddMessageToConsole("Command not recognized!!");
+                return;
+            }
+
+            if (!Commands.ContainsKey(_input[0]))
+            {
+                AddMessageToConsole("Command not recognized!!");
+            }
+            else
+            {
+                Commands[_input[0]].RunCommand();
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Prototyping/Movement/Scripts/PlayerMovement.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform playerModel;
+
+    [Header("Settings")]
+    public bool joystick = true;
+
+    [Header("Parameters")]
+    public float xySpeed = 18;
+    public float lookSpeed = 340;
+    public float forwardSpeed = 6;
+    public float invertUpDown = 1;
+    public bool sideRoll = false;
+
+    [Header("Public References")]
+    public Transform aimTarget;
+    //public CinemachineDollyCart dolly;
+    //public Transform cameraParent;
+    public float yMin = 0;
+    public float yMax = 1;
+    public float xMin = 0;
+    public float xMax = 1;
+    public Animator sideWindAnim;
+
+    void Start()
+    {
+        playerModel = transform.GetChild(0);
+    }
+
+
+    void Update()
+    {
+        float h = joystick ? Input.GetAxis("Horizontal") : Input.GetAxis("Mouse X");
+        float v = joystick ? Input.GetAxis("Vertical") : Input.GetAxis("Mouse Y");
+        LocalMove(h, v*invertUpDown, xySpeed);
+        RotationLook(h, v*invertUpDown, lookSpeed);
+        HorizontalLean(playerModel, h, 80, .1f);
+  
+
+
+        //sideRoll
+        if (Input.GetKeyDown(KeyCode.Q) && !sideRoll)
+        {
+            sideRoll = true;
+            sideWindAnim.SetBool("leanLeft", true);
+            SideWind(playerModel, -1, 1f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            sideRoll = false;
+            sideWindAnim.SetBool("leanLeft", false);
+            SideWind(playerModel, 0,  1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && !sideRoll)
+        {
+            sideRoll = true;
+            sideWindAnim.SetBool("leanRight", true);
+            SideWind(playerModel, 1, 1f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            sideRoll = false;
+            sideWindAnim.SetBool("leanRight", false);
+            SideWind(playerModel, 0, 1f);
+        }
+        //sideRoll END
+
+    }
+
+    void LocalMove(float x, float y, float speed)
+    {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+        ClampPosition();
+    }
+
+    void ClampPosition()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, xMin, xMax);
+        pos.y = Mathf.Clamp(pos.y, yMin, yMax);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    void RotationLook(float h, float v, float speed)
+    {
+        aimTarget.parent.position = Vector3.zero;
+        aimTarget.localPosition = new Vector3(h, v, 1);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed * Time.deltaTime);
+    }
+
+    void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
+    {
+        Vector3 targetEulerAngels = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
+    }
+
+    void SideWind(Transform target, int dir, float lerpTime)
+    {
+       
+        //Vector3 targetEulerAngels = target.localEulerAngles;
+        //target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -dir * 90, lerpTime));
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(aimTarget.position, .5f);
+        Gizmos.DrawSphere(aimTarget.position, .15f);
+    }
+}
